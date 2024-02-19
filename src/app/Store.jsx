c94b3e95370fc9703c42cd12545c8179baaebcde
SymuNadeem <msymunnadeem@gmail.com>
2024-02-19 13:36:31 +0600

    data fecthing toolkit

--- src/app/Store.jsx
+++ src/app/Store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterSlice from "./features/counter/CounterSlice";
+import PostSlice from "./features/posts/PostSlice";
 
 
 const store = configureStore({
   reducer: {
-    counter: CounterSlice
+    counter: CounterSlice,
+    posts: PostSlice
   }
 })
 export default store
